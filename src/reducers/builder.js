0ee8d22f8e09981d0480bc0ba0e60046ac960eb4
Taylor Wu <taylorwu21@gmail.com>
2017-07-25 19:53:23 -0600

    add redux

---
+++ src/reducers/builder.js
@@ -0,0 +1,12 @@
+const initialState = {
+  isworking: true,
+}
+
+const builder = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return { isWorking: true }
+  }
+}
+
+export default builder;
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import builder from './builder';
+
+const rootReducer = combineReducers({
+  builder,
+});
+
+export default rootReducer;
