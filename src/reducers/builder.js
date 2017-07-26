98bdf36a98e83b9628e4006307c467eddc9c4a3b
Taylor Wu <taylorwu21@gmail.com>
2017-07-25 20:04:29 -0600

    add eslint

--- src/reducers/builder.js
+++ src/reducers/builder.js
@@ -1,12 +1,12 @@
 const initialState = {
   isworking: true,
-}
+};
 
 const builder = (state = initialState, action) => {
   switch(action.type) {
     default:
-      return { isWorking: true }
+      return { isWorking: true };
   }
-}
+};
 
 export default builder;
--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, compose } from 'redux';
 
 import rootReducer from './reducers/index';
 
 const enhancers = compose(
-  window.devToolsExtension ? window.devToolsExtension() : f => f
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f
 );
 
 const store = createStore(rootReducer, {}, enhancers);
